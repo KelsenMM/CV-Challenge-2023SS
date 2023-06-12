bab4635020843d433269a119009bd07a4d06ea36
KelsenMM <zkc961024@gmail.com>
2023-06-12 17:10:25 +0200

    initial commit

--- Camera.m
+++ Camera.m
@@ -28,6 +28,22 @@ classdef Camera
                 camIntrincMat = [f_x,0,c_x;0,f_y,c_y;0,0,1];
             end
         end
+     
+        function [ cp_cell ] = makehat(mat_3xN)
+
+            cp_cell = cell(1, size(mat_3xN,2));
+            for i = 1:size(mat_3xN,2)
+            vec_hat = zeros(3,3);
+            vec_hat(1,2) = -mat_3xN(3, i);
+            vec_hat(1,3) = mat_3xN(2, i);
+            vec_hat(2,1) = mat_3xN(3, i);
+            vec_hat(2,3) = -mat_3xN(1, i);
+            vec_hat(3,1) = -mat_3xN(2, i);
+            vec_hat(3,2) = mat_3xN(2, i);
+            cp_cell{1,i} = vec_hat;
+            end
+        end
+    
     end
 end
 
